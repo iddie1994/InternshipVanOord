9ceb85313df1ac891bc75bbc6d6c56e049d58078
IMDijkstra <73649939+iddie1994@users.noreply.github.com>
2022-02-16 09:53:35 +0100

    Add files via upload

---
+++ Off_PolicyPlot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from ema_workbench import ema_logging, load_results
+from ema_workbench.analysis import envelopes, Density
+
+ema_logging.log_to_stderr(ema_logging.INFO)
+
+file_name = r'./data/12Wind6H2_DoggerBank_AEL_Island_NoSubsidy.tar.gz'
+experiments, outcomes = load_results(file_name)
+
+# the plotting functions return the figure and a dict of axes
+fig, axes = envelopes(experiments, outcomes, group_by='policy',
+                      density=Density.VIOLIN, fill=True)
+
+# we can access each of the axes and make changes
+for key, value in axes.items():
+    # the key is the name of the outcome for the normal plot
+    # and the name plus '_density' for the endstate distribution
+    if key.endswith('_density'):
+        value.set_xscale('log')
+
+plt.show()
