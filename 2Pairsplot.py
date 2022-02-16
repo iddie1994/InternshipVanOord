b7fbd89703636150fb5b20968e75a25b39d195c4
IMDijkstra <73649939+iddie1994@users.noreply.github.com>
2022-02-16 10:09:29 +0100

    Update and rename Pairsplot.py to 2Pairsplot.py

--- 2Pairsplot.py
+++ 2Pairsplot.py
@@ -9,26 +9,25 @@ from ema_workbench.analysis.pairs_plotting import (pairs_lines, pairs_scatter,
 
 ema_logging.log_to_stderr(level=ema_logging.DEFAULT_LEVEL)
 
-# load data
+# Load data
 fn = r'./data/windhydrogen100.tar.gz'
 experiments, outcomes = load_results(fn)
 
-# transform the results to the required format
-# that is, we want to know the max peak and the casualties at the end of the
-# run
+# Transform the results into the required format
+# which is; we want to know the max peak and the casualties at the end of the run
 tr = {}
 
 for key, value in outcomes.items():
     if key == 'Cumulative hydrogen production':
         tr[key] = value[:, -1]  # we want the end value
     else:
-        # we want the maximum value of the peak
+        # We want the maximum value of the peak
         max_peak = np.max(value, axis=1)
         tr['max peak'] = max_peak
 
-        # we want the time at which the maximum occurred
-        # the code here is a bit obscure, I don't know why the transpose
-        # of value is needed. This however does produce the appropriate results
+        # We'd like the time at which the maximum occurred
+        # The code is a bit obscure. Why the transpose
+        # of the value is needed is something I cannot explain. Nonetheless, it produces the correct results
         logical = value.T == np.max(value, axis=1)
 
 pairs_scatter(experiments, tr, filter_scalar=False)
